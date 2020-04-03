c2cf2a00fcf2cf4fda918ad032f3dab7384670ef
JojiiOfficial <jojii@jojii.de>
2020-04-03 14:31:10 +0200

    initial commit

---
+++ app/src/main/java/de/jojii/datamanagerandroid/MainActivity.java
@@ -0,0 +1,27 @@
+package de.jojii.datamanagerandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.btn_click);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
---
+++ go/test/test.go
@@ -0,0 +1,5 @@
+package test
+
+func Tst() string {
+	return "lol"
+}
